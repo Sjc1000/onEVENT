a8ec578cb958edc5f21c19526bc3800bbb1fb92a
Steven James Core <42Echo6Alpha@gmail.com>
2015-04-02 16:13:08 +1100

    Added description

--- events/procexists.py
+++ events/procexists.py
@@ -1,6 +1,14 @@
 import subprocess
 
 def procexists(process):
+	'''procexists
+	Checks if a process exists
+	params:
+		- process - The name of the process to search for.
+	
+	returns:
+		- pid - A list of PID's found for the process, if there are more than 1.
+	'''
 	proc = subprocess.Popen(['ps', '-e'], stdout=subprocess.PIPE)
 	grep = subprocess.Popen(['grep', '.*' + process + '.*'], stdin=proc.stdout, stdout=subprocess.PIPE)
 	try:
@@ -12,3 +20,6 @@ def procexists(process):
 		return (1,) + tuple(pid)
 	return (0, 'None')
 
+
+if __name__ == '__main__':
+	help(procexists)
