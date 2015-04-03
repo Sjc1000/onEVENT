e7ccf8c23e1adecad6b2ffb886f7b9a292109afa
Sjc1000 <steven@uptone-software.(none)>
2015-04-03 19:37:38 +1100

    Changed how battery loads info

--- events/battery.py
+++ events/battery.py
@@ -6,25 +6,22 @@ def getbatteryinfo():
 	battery = {}
 	for folder in os.listdir('/sys/class/power_supply'):
 		if 'BAT' in folder:
-			batfolder = '/sys/class/power_supply/' + folder
+			batfolder = '/sys/class/power_supply/' + folder + '/'
 			break
 
-	with open(batfolder + '/uevent') as ufile:
-		battery_info = ufile.read()
-	
-	for item in battery_info.split('\n'):
-		if item == '':
-			continue
-		battery[item.split('=')[0]] = item.split('=')[1]
+	_files = os.listdir(batfolder)
+	for f in _files:
+		if os.path.isfile(batfolder+f):
+			with open(batfolder + f, 'r') as tfile:
+				battery[f] = tfile.read().split('\n')[0]
 	return battery
 
 def getbatterylevel():
 	battery = getbatteryinfo()
-	full = battery['POWER_SUPPLY_CHARGE_FULL']
-	current = battery['POWER_SUPPLY_CHARGE_NOW']
-	cap = battery['POWER_SUPPLY_CAPACITY']
-	perc = round((float(full) / float(current)) * float(cap))
-	return int(perc)
+	current = battery['charge_now']
+	full = battery['charge_full']
+	perc = round(int(current) / int(full) * 100 )
+	return perc
 
 def _is(level):
 	return (getbatterylevel() == int(level), getbatterylevel())
@@ -40,11 +37,11 @@ def _larger(amount):
 
 def _charging():
 	battery = getbatteryinfo()
-	return (battery['POWER_SUPPLY_STATUS'] == 'Charging' or battery['POWER_SUPPLY_STATUS'] == 'Full', getbatterylevel())
+	return (battery['status'] == 'Charging' or battery['status'] == 'Full', getbatterylevel())
 
 def _full():
 	battery = getbatteryinfo()
-	return (battery['POWER_SUPPLY_STATUS'] == 'Full', getbatterylevel())
+	return (battery['status'] == 'Full', getbatterylevel())
 
 def battery(function, *params):
 	'''battery
@@ -55,7 +52,6 @@ def battery(function, *params):
 	if '_' + function in globals():
 		return globals()['_'+function](*params)
 	return (0, 'None')
-
 
 if __name__ == '__main__':
 	help(battery)
